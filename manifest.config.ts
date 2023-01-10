b6a1b4699a105cde1d76e38e807682383af144fb
Николай Миронов <poeshonya3@gmail.com>
2023-01-10 22:38:26 +0300

    v0.2.7

--- manifest.config.ts
+++ manifest.config.ts
@@ -2,7 +2,7 @@ import { defineManifest, ManifestV3Export } from '@crxjs/vite-plugin';
 
 const manifest: ManifestV3Export = {
 	name: 'Path of exile blocklist',
-	version: '0.2.6',
+	version: '0.2.7',
 	manifest_version: 3,
 	permissions: ['storage'],
 	description: 'Hide posts from unwanted users on PoE Forum',
