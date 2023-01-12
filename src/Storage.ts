88bb8d909b0154026e5f50a8c291a4f09323668e
Николай Миронов <poeshonya3@gmail.com>
2023-01-12 23:33:37 +0300

    improve addUser and typings in Storage

--- src/Storage.ts
+++ src/Storage.ts
@@ -19,16 +19,15 @@ const getOrDefault = async <TKey extends keyof StorageItems>(
 
 const clearAll = async (): Promise<void> => chrome.storage.sync.clear();
 
-const addUser = async (user: string) => {
+const addUser = async (user: string): Promise<void> => {
 	const users = await getOrDefault('users', []);
-	const hasThisUser = Boolean(users.find(u => u.toLowerCase() === user.toLowerCase()));
-	console.log(hasThisUser);
+	const hasThisUser = users.some(u => u.toLowerCase() === user.toLowerCase());
 	if (hasThisUser) return;
 	users.push(user);
 	set('users', users);
 };
 
-const removeUser = async (user: string) => {
+const removeUser = async (user: string): Promise<void> => {
 	let users = await getOrDefault('users', []);
 	users = users.filter(u => u !== user);
 	set('users', users);
@@ -43,32 +42,3 @@ export const Storage = {
 	addUser,
 	removeUser,
 };
-
-// export class Storage {
-// 	static async get() {
-// 		return chrome.storage.sync.get();
-// 	}
-// 	static async set<Key extends keyof StorageItems>(key: Key, value: StorageItems[Key]): Promise<void> {
-// 		chrome.storage.sync.set({ [`${key}`]: value });
-// 	}
-
-// 	static async getOrDefault<Key extends keyof StorageItems>(
-// 		key: Key,
-// 		defaultValue: StorageItems[Key]
-// 	): Promise<StorageItems[Key]> {
-// 		const resultObject = await chrome.storage.sync.get(key);
-// 		if (!Object.hasOwn(resultObject, key)) {
-// 			return defaultValue;
-// 		}
-
-// 		return resultObject[key];
-// 	}
-
-// 	static async clearAll() {
-// 		return chrome.storage.sync.clear();
-// 	}
-
-// 	static async delete(key: keyof StorageItems) {
-// 		return chrome.storage.sync.remove(key);
-// 	}
-// }
