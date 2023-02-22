104742b473a9f1cc78ca7790c5ad469f57be894e
Николай Миронов <poeshonya3@gmail.com>
2023-02-22 20:52:46 +0300

    load lang from localStorage to prevent language flickering

--- src/options/useSettings.ts
+++ src/options/useSettings.ts
@@ -2,19 +2,33 @@ import { ref, watch, onMounted } from 'vue';
 import { PostStyle, SupportedLang } from '../types';
 import { Storage } from '../Storage';
 import { Default } from '../Default';
+import { Option } from '../types';
+
+const LOCAL_STORAGE_LANG_KEY = 'lang';
+
+const getLocalStorageLang = (): Option<SupportedLang> => {
+	const lang = localStorage.getItem(LOCAL_STORAGE_LANG_KEY);
+	if (lang !== 'ru' && lang !== 'en') return null;
+	return lang;
+};
 
 export const useSettings = () => {
 	const postStyle = ref<PostStyle>(Default.postStyle);
-	const lang = ref<SupportedLang>(Default.lang());
+	const lang = ref<SupportedLang>(getLocalStorageLang() ?? Default.lang());
 	const withIcons = ref<boolean>(Default.withIcons);
 
 	onMounted(async () => {
 		try {
-			Promise.all([
-				(postStyle.value = await Storage.getOrDefault('postStyle', Default.postStyle)),
-				(lang.value = await Storage.getOrDefault('lang', Default.lang())),
-				(withIcons.value = await Storage.getOrDefault('withIcons', Default.withIcons)),
+			const [_postStyle, _lang, _withIcons] = await Promise.all([
+				Storage.getOrDefault('postStyle', Default.postStyle),
+				Storage.getOrDefault('lang', getLocalStorageLang() ?? Default.lang()),
+				Storage.getOrDefault('withIcons', Default.withIcons),
 			]);
+
+			postStyle.value = _postStyle;
+			lang.value = _lang;
+			withIcons.value = _withIcons;
+			localStorage.setItem(LOCAL_STORAGE_LANG_KEY, _lang);
 		} catch (err) {
 			console.log(err);
 		}
@@ -30,6 +44,7 @@ export const useSettings = () => {
 		val => {
 			if (val !== 'en' && val !== 'ru') return;
 			Storage.set('lang', val);
+			localStorage.setItem(LOCAL_STORAGE_LANG_KEY, val);
 		}
 	);
 
