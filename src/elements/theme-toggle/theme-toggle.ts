fa8a11e29d5ac8dbda21c47819f4ff1c9387be1a
shonya3 <poeshonya3@gmail.com>
2023-03-07 23:38:43 +0300

    update theme-toggle to work with dsd

--- src/elements/theme-toggle/theme-toggle.ts
+++ src/elements/theme-toggle/theme-toggle.ts
@@ -4,6 +4,7 @@ const css = new CSSStyleSheet();
 css.replaceSync(styles);
 
 import sunmoonStyles from './sunmoon.css?inline';
+import { template as themeToggleTemplate } from './template';
 const sunmoonCss = new CSSStyleSheet();
 sunmoonCss.replaceSync(sunmoonStyles);
 
@@ -66,38 +67,17 @@ export class ThemeToggle extends HTMLElement {
 	#shadowRoot: ShadowRoot;
 	constructor() {
 		super();
-		const template = document.createElement('template');
-		template.innerHTML = String.raw`
-            <button
-                class="theme-toggle"
-                id="theme-toggle"
-                title="Toggles light & dark"
-                aria-label="auto"
-                aria-live="polite"
-		    >
-                <svg class="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
-                    <mask class="moon" id="moon-mask">
-                        <rect x="0" y="0" width="100%" height="100%" fill="white" />
-                        <circle cx="24" cy="10" r="6" fill="black" />
-                    </mask>
-                    <circle class="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
-                    <g class="sun-beams" stroke="currentColor">
-                        <line x1="12" y1="1" x2="12" y2="3" />
-                        <line x1="12" y1="21" x2="12" y2="23" />
-                        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-                        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-                        <line x1="1" y1="12" x2="3" y2="12" />
-                        <line x1="21" y1="12" x2="23" y2="12" />
-                        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-                        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-                    </g>
-                </svg>
-		    </button>
-        `;
+		const { shadowRoot } = this.attachInternals();
+		if (!shadowRoot) {
+			const template = document.createElement('template');
+			template.innerHTML = themeToggleTemplate;
 
-		this.#shadowRoot = this.attachShadow({ mode: 'open' });
-		this.#shadowRoot.adoptedStyleSheets = [css, sunmoonCss];
-		this.#shadowRoot.append(template.content.cloneNode(true));
+			this.#shadowRoot = this.attachShadow({ mode: 'open' });
+			this.#shadowRoot.adoptedStyleSheets = [css, sunmoonCss];
+			this.#shadowRoot.append(template.content.cloneNode(true));
+		} else {
+			this.#shadowRoot = shadowRoot;
+		}
 
 		this.$button?.addEventListener('click', this.toggleTheme.bind(this));
 	}
