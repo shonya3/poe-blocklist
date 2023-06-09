6662830f7922381c7e8f73826d89634c4fea538a
shonya3 <poeshonya3@gmail.com>
2023-06-09 17:59:41 +0300

    deps

--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -3,6 +3,21 @@ import { Hide } from './hide/mod';
 import { Storage } from '../Storage';
 import { Update } from './update/mod';
 import { Default } from '../Default';
+import { Character } from './character/mod';
+import { $ } from './$';
+
+export const getCharacterInfo = async (accountName: string, character: string) => {
+	const { origin } = window.location;
+	const href = `${origin}/character-window/get-characters`;
+	const url = new URL(href);
+
+	url.searchParams.append('accountName', accountName);
+	url.searchParams.append('character', character);
+
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
 
 async function main(): Promise<void> {
 	try {
@@ -16,6 +31,13 @@ async function main(): Promise<void> {
 
 		Update.posts.addBlockUserIcon(lang);
 		Hide.by({ keywords, users }, postStyle, lang, withIcons);
+
+		const uniqueVisibleNames = $.uniqueUsernames($.visiblePosts());
+
+		console.time('api-call-for-active-characters');
+		const activeCharacters = await Character.info(uniqueVisibleNames);
+		console.timeEnd('api-call-for-active-characters');
+		console.log(activeCharacters);
 	} catch (err) {
 		console.log(err);
 	}
