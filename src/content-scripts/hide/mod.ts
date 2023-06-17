b959ce49b0ec3d5c0bc8e87ff733b278d6d8e256
shonya3 <poeshonya3@gmail.com>
2023-06-17 11:55:56 +0300

    fix: hide the thread with "blocked" posted by

--- src/content-scripts/hide/mod.ts
+++ src/content-scripts/hide/mod.ts
@@ -7,7 +7,7 @@ export const threads = (users: SearchData['users'] = []): void => {
 	if (!$.thread.isThreadsView()) return;
 	for (const thread of $.threads()) {
 		const createdBy = $.thread.createdBy(thread);
-		if (createdBy && users.includes(createdBy)) {
+		if ((createdBy && users.includes(createdBy)) || createdBy === 'blocked') {
 			hideElement(thread);
 		}
 	}
