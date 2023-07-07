805951385ec37cdf7e2db04ebe4186ffd247487d
shonya3 <poeshonya3@gmail.com>
2023-07-07 10:34:33 +0300

    cleanup old post api

--- src/content-scripts/Dom/mod.ts
+++ src/content-scripts/Dom/mod.ts
@@ -4,35 +4,6 @@ import { Thread } from './Thread';
 import { Forum } from './Forum';
 import { Post } from './Post';
 
-// const post = {
-// 	buttons(post: Element): Option<HTMLElement> {
-// 		return post.querySelector('div.buttons');
-// 	},
-// 	username(post: Element): Option<string> {
-// 		return post.querySelector('span.profile-link.post_by_account > a')?.textContent ?? null;
-// 	},
-
-// 	contentCell(post: Element): Option<HTMLElement> {
-// 		return post.querySelector('.content-container');
-// 	},
-
-// 	profileCell(post: Element): Option<HTMLElement> {
-// 		return post.querySelector('.post_info');
-// 	},
-
-// 	content(post: Element): Option<HTMLElement> {
-// 		return post.querySelector('.content');
-// 	},
-
-// 	children(post: Element): HTMLTableCellElement[] {
-// 		return Array.from(post.querySelectorAll('td'));
-// 	},
-
-// 	isPostHidden(post: Element) {
-// 		return Boolean(post.querySelector(':has(blocked-content)'));
-// 	},
-// };
-
 const quote = {
 	quotationMarks(quote: Element): Option<HTMLElement> {
 		return quote.querySelector('span.quote');
@@ -50,10 +21,6 @@ const quote = {
 };
 
 export const $ = {
-	// posts(): HTMLElement[] {
-	// 	return Array.from(document.querySelectorAll('tr:has(.content)'));
-	// },
-
 	quotes(): HTMLQuoteElement[] {
 		return Array.from(document.querySelectorAll('blockquote'));
 	},
